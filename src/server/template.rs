0e03a182e2353cc61cde02db748c6b6a6ae1d376
mcbarinov <mcbarinov@proton.me>
2024-08-05 09:18:26 +0300

    dt filter #18

--- src/server/template.rs
+++ src/server/template.rs
@@ -1,6 +1,8 @@
 use axum::response::Html;
+use chrono::DateTime;
 use minijinja::value::ViaDeserialize;
 use minijinja::{Environment, Value};
+use serde::de::Error;
 use serde::Serialize;
 
 use crate::db::Proxy;
@@ -9,6 +11,7 @@ use crate::Config;
 
 pub fn init_templates(config: &Config) -> Environment<'static> {
     let mut env = Environment::new();
+    minijinja_contrib::add_to_environment(&mut env);
     minijinja_embed::load_templates!(&mut env);
     env.add_filter("none", none);
     env.add_filter("dt", dt);
@@ -27,8 +30,17 @@ pub fn none(value: Value) -> Value {
     }
 }
 
-pub fn dt(value: Value) -> Value {
-    value
+pub fn dt(value: Value) -> Result<String, minijinja::Error> {
+    if value.is_undefined() || value.is_none() {
+        Ok("".to_string())
+    } else {
+        let res = DateTime::parse_from_rfc3339(&value.to_string())
+            .map_err(|_| minijinja::Error::custom("dt filter failed to parse datetime"))?
+            .to_utc()
+            .format("%Y-%m-%d %H:%M:%S")
+            .to_string();
+        Ok(res)
+    }
 }
 
 fn history_ok_count(proxy: ViaDeserialize<Proxy>) -> usize {
