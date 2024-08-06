7b7454548f08a7c28749f781a0d5bb2f8647e644
mcbarinov <mcbarinov@proton.me>
2024-08-06 13:12:26 +0300

    mm_base2::init_tracing #22

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,25 +1,11 @@
 use std::sync::Arc;
 
-use tracing::Level;
-use tracing_subscriber::fmt::writer::MakeWriterExt;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{fmt, EnvFilter, Layer};
-
 use proxy_checker::{run_scheduler, serve_server, App, Config};
 
 #[tokio::main]
 async fn main() {
     let config = Config::new();
-
-    let (non_blocking, _guard) = tracing_appender::non_blocking(tracing_appender::rolling::never(&config.data_dir, "app.log"));
-    let file_layer = fmt::Layer::new().with_writer(non_blocking.with_max_level(Level::WARN)).json();
-    let console_layer = fmt::layer()
-        .with_line_number(true)
-        .with_thread_ids(true)
-        .with_file(true)
-        .with_filter(EnvFilter::from("proxy_checker=debug"));
-    tracing_subscriber::registry().with(file_layer).with(console_layer).init();
+    mm_base2::init_tracing("proxy_checker=debug", config.data_dir.as_str());
 
     let app = Arc::new(App::new(&config).await);
     run_scheduler(Arc::clone(&app));
